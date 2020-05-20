fab9e3577affdd87cfad33a519dbbb07579bc690
Rody10 <rodneysimbatholanah@gmail.com>
2020-05-20 19:54:07 +0200

    first

--- ICPC/Program.fs
+++ ICPC/Program.fs
@@ -2,7 +2,39 @@
 open System
 
 let commaSprinkler input =
-    failwith "Not implemented"
+
+    
+    (*let checkCharacters (str: string) =
+        let k = Seq.toList str
+        match k with
+        |_ -> None
+        |_ -> None*)
+
+
+
+
+    
+    let validate (str:string) =
+        match (str.Length < 2) with
+        |true -> None
+        |false -> let k = Seq.toList str
+                  match List.exists (fun c -> not (Char.IsLower c) || not(c = ' ') || not(c = '.') || not(c = '.') ) k   with
+                  |true -> None
+                  |_ -> Some k
+
+        
+        
+    
+
+
+    validate input //|> checkCharacters
+
+
+
+
+
+
+
 
 let rivers input =
     failwith "Not implemented"
