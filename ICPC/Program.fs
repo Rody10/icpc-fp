e910afc802bda1708240bdff5514323973e3c8a7
Rody10 <rodneysimbatholanah@gmail.com>
2020-05-21 21:23:21 +0200

    passed 10

--- ICPC/Program.fs
+++ ICPC/Program.fs
@@ -22,8 +22,33 @@ let commaSprinkler input =
                   |true -> None
                   |_ -> Some k
 
+
+   (* let convertIntoWordsList (ogList: string List) = 
+        let nwList = []
+        let rec createWord  ogList nwList str =                                                         //str::' '.ToString::nwList  createWord
+            match ogList with
+            |a :: rest -> match a with
+                          |' ' -> str::' '.ToString::nwList (createWord rest nwList "")
+                          |'.' -> str::'.'.ToString::nwList (createWord rest nwList "")
+                          |_ -> str+a.ToString (createWord rest nwList str)
+            |_ -> nwList
+
+        convertIntoWordsList ogList nwList ""*)
+
+
+   
+
+
+
+
+
+
+    //let findCommas (lst:string list) =()
         
         
+
+   // let findWord (input:string) =
+      //  let lst = Seq.splitInto 
     
 
 
@@ -32,12 +57,20 @@ let commaSprinkler input =
 
 
 
+let rivers (input: string) =
+    let validate (str:string) =
+        let nList = input.Split ' ' 
+        match nList.Length<2 || (Seq.exists (fun elem -> elem = "") nList) || ((Seq.exists (fun (elem:string) -> elem.Length >80) nList)) || ((Seq.exists (fun (elem:string) -> elem.Contains "!") nList )) ||  ((Seq.exists (fun (elem:string) -> elem.Contains ",") nList ))  with
+        |true -> None
+        |false -> Some nList
 
 
 
 
-let rivers input =
-    failwith "Not implemented"
+        
+    validate input
+
+
 
 [<EntryPoint>]
 let main argv =
